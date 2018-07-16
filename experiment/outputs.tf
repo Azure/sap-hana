7fedaf462375e2f293848d02160134366db65668
Page Bowers <pabowers@microsoft.com>
2018-07-16 10:38:04 -0700

    Moved outputs to their own file

--- experiment/outputs.tf
+++ experiment/outputs.tf
@@ -1,4 +1,4 @@
 output "ip" {
-  value = "Created vm ${azurerm_virtual_machine.db0.id}"
+  value = "Created vm ${azurerm_virtual_machine.db.id}"
   value = "Connect using ${var.vm_user}@${local.vmFqdn}"
 }
