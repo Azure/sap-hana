bd72454e265268bf5a1ae4933b0b127c5e9c3750
Page Bowers <pabowers@microsoft.com>
2018-11-09 15:37:42 -0800

    NSG creation moved to Ansible, but called from Terraform. Specific IP addresses can be added to be allowed, or the default of opening everything can be used. The NSG for the windows bastion host is still created in Terraform, but the ability to customize the ip ranges is there.

--- deploy/vm/modules/nsg_for_hana/outputs.tf
+++ deploy/vm/modules/nsg_for_hana/outputs.tf
@@ -1,7 +1,9 @@
-output "nsg-id" {
-  value = "${azurerm_network_security_group.sap-nsg.*.id}"
+output "nsg-name" {
+  depends_on = ["null_resource.create-nsg"]
+  value      = "${var.nsg_name}"
 }
 
-output "nsg-name" {
-  value = "${element(concat(azurerm_network_security_group.sap-nsg.*.name,list(local.empty_string)),0)}"
-}
+# output "nsg-name" {
+#   value = "${element(concat(azurerm_network_security_group.sap-nsg.*.name,list(local.empty_string)),0)}"
+# }
+
