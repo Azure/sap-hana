6baa2977e8d20a14f4c29eef3a1cb927659e2dd6
Page Bowers <pabowers@microsoft.com>
2018-11-13 09:07:34 -0800

    Removing the README.md files greated by ansible-galaxy init

--- deploy/vm/modules/nsg_for_hana/outputs.tf
+++ deploy/vm/modules/nsg_for_hana/outputs.tf
@@ -2,8 +2,3 @@ output "nsg-name" {
   depends_on = ["null_resource.create-nsg"]
   value      = "${var.nsg_name}"
 }
-
-# output "nsg-name" {
-#   value = "${element(concat(azurerm_network_security_group.sap-nsg.*.name,list(local.empty_string)),0)}"
-# }
-
