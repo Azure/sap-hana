9486a2c7be7bafef3f01ad69e8d23febc05ca54b
Page Bowers <pabowers@microsoft.com>
2018-11-13 09:07:34 -0800

    Fixed issue with allow_ips
    Ports for all rules are opened for the machines being created
    
    Ports and ip addresses get added

--- deploy/vm/modules/nsg_for_hana/outputs.tf
+++ deploy/vm/modules/nsg_for_hana/outputs.tf
@@ -1,9 +1,4 @@
 output "nsg-name" {
-  depends_on = ["null_resource.create-nsg"]
+  depends_on = ["null_resource.create-nsg", "null_resource.destroy-nsg"]
   value      = "${var.nsg_name}"
 }
-
-# output "nsg-name" {
-#   value = "${element(concat(azurerm_network_security_group.sap-nsg.*.name,list(local.empty_string)),0)}"
-# }
-
