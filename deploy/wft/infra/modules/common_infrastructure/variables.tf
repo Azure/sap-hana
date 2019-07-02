7f63a2a7982b2d22737cd964929ee20941badbd1
Suhan Selvan <suhans@wftus.com>
2019-07-02 04:04:07 +0000

    new module structure

--- deploy/wft/infra/modules/common_infrastructure/variables.tf
+++ deploy/wft/infra/modules/common_infrastructure/variables.tf
@@ -1,43 +1,57 @@
 variable "az_region" {
+  description	= "Which Azure region to deploy the HANA setup into. e.g. <eastus>"
 }
 
 variable "az_resource_group" {
+  description	= "Which Azure resource group to deploy the HANA setup into.  i.e. <myResourceGroup>"
 }
 
 variable "az_hub_vnet" {
+  description	= "Name of the Hub VNET to deploy the Jumpbox into"
 }
 
 variable "az_hub_address_space" {
+  description	= "Address space of the Hub VNET"
 }
 
 variable "az_hub_subnet" {
+  description	= "Name of the Hub Subnet to deploy the Jumpbox"
 }
 
 variable "az_hub_subnet_prefix" {
+  description	= "Address Prefix of the Hub Subnet"
 }
 
 variable "az_spoke_vnet" {
+  description	= "Name of the spoke vnet"
 }
 
 variable "az_spoke_address_space" {
+  description	= "Address space of the spoke vnet"
 }
 
 variable "az_spoke_db_client_subnet" {
-}
-
-variable "az_spoke_db_client_subnet_prefix" {
+  description	= "Name of the spoke DB client Subnet"
 }
 
 variable "az_spoke_db_admin_subnet" {
+  description	= "Name of the spoke DB admin Subnet"
+}
+
+variable "az_spoke_db_client_subnet_prefix" {
+  description	= "Address Prefix of the Spoke DB Client Subnet"
 }
 
 variable "az_spoke_db_admin_subnet_prefix" {
+  description	= "Address Prefix of the Spoke DB Admin Subnet"
 }
 
 variable "az_jumpbox_os" {
+  description	= "Operating system of the Jumpbox"
 }
 
 variable "az_jumpbox_private_ip_address" {
+  description	= "Private ip address of the Jumpbox"
 }
 
 variable "az_vm" {
