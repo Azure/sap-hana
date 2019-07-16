864089c8c4eb75050c7d24210788e7ea03261eb0
Suhan Selvan <suhans@wftus.com>
2019-07-16 10:00:48 +0000

    v2:create common infrastructure resources

--- deploy/v2/terraform/modules/common_infrastructure/main.tf
+++ deploy/v2/terraform/modules/common_infrastructure/main.tf
@@ -1,3 +1,11 @@
+##################################################################################################################
+# RESOURCES
+##################################################################################################################
+
+
+# RESOURCE GROUP =================================================================================================
+
+
 # Creates the resource group
 resource "azurerm_resource_group" "rg" {
   count    = var.resource_group_existing ? 0 : 1
@@ -5,7 +13,11 @@ resource "azurerm_resource_group" "rg" {
   location = var.region
 }
 
-# Creates the management vnet 
+
+# VNETs ==========================================================================================================
+
+
+# Creates the management vnet
 resource "azurerm_virtual_network" "mgmt-vnet" {
   count               = var.mgmt_vnet_existing ? 0 : 1
   name                = var.mgmt_vnet_name
@@ -22,6 +34,24 @@ resource "azurerm_virtual_network" "sap-vnet" {
   resource_group_name = var.resource_group_existing ? var.resource_group_name : azurerm_resource_group.rg[0].name
   address_space       = [var.sap_vnet_address_space]
 }
+
+# Imports existing management vnet data
+data "azurerm_virtual_network" "mgmt-vnet" {
+  count               = var.mgmt_vnet_existing ? 1 : 0
+  name                = var.mgmt_vnet_name
+  resource_group_name = var.resource_group_name
+}
+
+# Imports existing sap vnet data
+data "azurerm_virtual_network" "sap-vnet" {
+  count               = var.sap_vnet_existing ? 1 : 0
+  name                = var.sap_vnet_name
+  resource_group_name = var.resource_group_name
+}
+
+
+# SUBNETs ========================================================================================================
+
 
 # Creates the management default subnet
 resource "azurerm_subnet" "mgmt-default-subnet" {
@@ -59,6 +89,10 @@ resource "azurerm_subnet" "sap-app-subnet" {
   address_prefix       = var.sap_app_subnet_prefix
 }
 
+
+# NSGs ===========================================================================================================
+
+
 # Creates management default subnet nsg
 resource "azurerm_network_security_group" "mgmt-default-nsg" {
   count               = var.mgmt_default_nsg_existing ? 0 : 1
@@ -91,34 +125,25 @@ resource "azurerm_network_security_group" "sap-app-nsg" {
   resource_group_name = var.resource_group_existing ? var.resource_group_name : azurerm_resource_group.rg[0].name
 }
 
-# Imports existing management vnet data
-data "azurerm_virtual_network" "mgmt-vnet" {
-  count               = var.mgmt_vnet_existing ? 1 : 0
-  name                = var.mgmt_vnet_name
-  resource_group_name = var.resource_group_name
-}
 
-# Imports existing sap vnet data
-data "azurerm_virtual_network" "sap-vnet" {
-  count               = var.sap_vnet_existing ? 1 : 0
-  name                = var.sap_vnet_name
-  resource_group_name = var.resource_group_name
-}
+# VNET PEERINGs ==================================================================================================
+
 
 # Peers management vnet to sap vnet
 resource "azurerm_virtual_network_peering" "mgmt-sap-peering" {
   count                        = signum((var.mgmt_vnet_existing ? 0 : 1) + (var.sap_vnet_existing ? 0 : 1))
-  name                         = "${var.mgmt_vnet_name}-${var.sap_vnet_name}-peering"
+  name                         = "${var.mgmt_vnet_name}-${var.sap_vnet_name}"
   resource_group_name          = var.resource_group_name
   virtual_network_name         = var.mgmt_vnet_existing ? var.mgmt_vnet_name : azurerm_virtual_network.mgmt-vnet[0].name
   remote_virtual_network_id    = var.sap_vnet_existing ? data.azurerm_virtual_network.sap-vnet[0].id : azurerm_virtual_network.sap-vnet[0].id
   allow_virtual_network_access = true
 }
 
+
 # Peers sap vnet to management vnet
 resource "azurerm_virtual_network_peering" "sap-mgmt-peering" {
   count                        = signum((var.mgmt_vnet_existing ? 0 : 1) + (var.sap_vnet_existing ? 0 : 1))
-  name                         = "${var.sap_vnet_name}-${var.mgmt_vnet_name}-peering"
+  name                         = "${var.sap_vnet_name}-${var.mgmt_vnet_name}"
   resource_group_name          = var.resource_group_name
   virtual_network_name         = var.sap_vnet_existing ? var.sap_vnet_name : azurerm_virtual_network.sap-vnet[0].name
   remote_virtual_network_id    = var.mgmt_vnet_existing ? data.azurerm_virtual_network.mgmt-vnet[0].id : azurerm_virtual_network.mgmt-vnet[0].id
