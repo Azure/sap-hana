696e12cd1288b6f310d1f0c6eefe8c853e677b87
Suhan Selvan <suhans@wftus.com>
2019-07-16 08:23:12 +0000

    new module structure

---
+++ deploy/v2/terraform/modules/common_infrastructure/variables.tf
@@ -0,0 +1,115 @@
+variable "region" {
+  description = "Azure region to deploy the sap setup"
+}
+
+variable "resource_group_existing" {
+  description = "Use existing resource group"
+}
+
+variable "resource_group_name" {
+  description = "Name of the azure resource group to deploy the sap setup"
+}
+
+variable "mgmt_vnet_existing" {
+  description = "Use existing management vnet"
+}
+
+variable "mgmt_vnet_name" {
+  description = "Name of the management vnet"
+}
+
+variable "mgmt_vnet_address_space" {
+  description = "Address space of the management vnet"
+}
+
+variable "sap_vnet_existing" {
+  description = "Use existing sap vnet"
+}
+
+variable "sap_vnet_name" {
+  description = "Name of the sap vnet"
+}
+
+variable "sap_vnet_address_space" {
+  description = "Address space of the sap vnet"
+}
+
+variable "mgmt_default_subnet_existing" {
+  description = "Use existing management subnet"
+}
+
+variable "mgmt_default_subnet_name" {
+  description = "Name of the management subnet"
+}
+
+variable "mgmt_default_subnet_prefix" {
+  description = "Address prefix of the management subnet"
+}
+
+variable "sap_client_subnet_existing" {
+  description = "Use existing sap client subnet"
+}
+
+variable "sap_client_subnet_name" {
+  description = "Name of the sap client subnet"
+}
+
+variable "sap_client_subnet_prefix" {
+  description = "Address prefix of the sap client subnet"
+}
+
+variable "sap_admin_subnet_existing" {
+  description = "Use existing sap admin subnet"
+}
+
+variable "sap_admin_subnet_name" {
+  description = "Name of the sap admin subnet"
+}
+
+variable "sap_admin_subnet_prefix" {
+  description = "Address prefix of the sap admin subnet"
+}
+
+variable "sap_app_subnet_existing" {
+  description = "Use existing sap application subnet"
+}
+
+variable "sap_app_subnet_name" {
+  description = "Name of the sap application subnet"
+}
+
+variable "sap_app_subnet_prefix" {
+  description = "Address prefix of the sap application subnet"
+}
+
+variable "mgmt_default_nsg_existing" {
+  description = "Use existing management nsg"
+}
+
+variable "mgmt_default_nsg_name" {
+  description = "Name of the management nsg"
+}
+
+variable "sap_client_nsg_existing" {
+  description = "Use existing sap client nsg"
+}
+
+variable "sap_client_nsg_name" {
+  description = "Name of the sap client nsg"
+}
+
+variable "sap_admin_nsg_existing" {
+  description = "Use existing sap admin nsg"
+}
+
+variable "sap_admin_nsg_name" {
+  description = "Name of the sap admin nsg"
+}
+
+variable "sap_app_nsg_existing" {
+  description = "Use existing sap application nsg"
+}
+
+variable "sap_app_nsg_name" {
+  description = "Name of the sap application nsg"
+}
---
+++ deploy/v2/terraform/modules/single_node_hana/variables.tf
@@ -0,0 +1,3 @@
+variable "infrastructure" {
+  description = "Details of the azure infrastucture required for the deployment"
+}
