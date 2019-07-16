1fbbff6598cacffdba5ab85d35c83ede1ed86a53
Suhan Selvan <suhans@wftus.com>
2019-07-16 09:01:35 +0000

    v2:create common infrastructure resources

--- deploy/v2/terraform/modules/single_node_hana/main.tf
+++ deploy/v2/terraform/modules/single_node_hana/main.tf
@@ -1,4 +1,4 @@
-# Initalizes Azure modules and authenticates to subscription
+# Initalizes azure modules and authenticates to subscription
 provider "azurerm" {
   version = "~> 1.30.1"
 }
@@ -18,21 +18,21 @@ module "common_infrastructure" {
   mgmt_default_subnet_existing = var.infrastructure.vnets.management.default_subnet.is_existing
   mgmt_default_subnet_name     = var.infrastructure.vnets.management.default_subnet.name
   mgmt_default_subnet_prefix   = var.infrastructure.vnets.management.default_subnet.prefix
-  sap_admin_subnet_existing    = var.infrastructure.vnets.sap.db_admin_subnet.is_existing
-  sap_admin_subnet_name        = var.infrastructure.vnets.sap.db_admin_subnet.name
-  sap_admin_subnet_prefix      = var.infrastructure.vnets.sap.db_admin_subnet.prefix
-  sap_client_subnet_existing   = var.infrastructure.vnets.sap.db_client_subnet.is_existing
-  sap_client_subnet_name       = var.infrastructure.vnets.sap.db_client_subnet.name
-  sap_client_subnet_prefix     = var.infrastructure.vnets.sap.db_client_subnet.prefix
+  sap_admin_subnet_existing    = var.infrastructure.vnets.sap.admin_subnet.is_existing
+  sap_admin_subnet_name        = var.infrastructure.vnets.sap.admin_subnet.name
+  sap_admin_subnet_prefix      = var.infrastructure.vnets.sap.admin_subnet.prefix
+  sap_client_subnet_existing   = var.infrastructure.vnets.sap.client_subnet.is_existing
+  sap_client_subnet_name       = var.infrastructure.vnets.sap.client_subnet.name
+  sap_client_subnet_prefix     = var.infrastructure.vnets.sap.client_subnet.prefix
   sap_app_subnet_existing      = var.infrastructure.vnets.sap.app_subnet.is_existing
   sap_app_subnet_name          = var.infrastructure.vnets.sap.app_subnet.name
   sap_app_subnet_prefix        = var.infrastructure.vnets.sap.app_subnet.prefix
   mgmt_default_nsg_existing    = var.infrastructure.vnets.management.default_subnet.nsg.is_existing
   mgmt_default_nsg_name        = var.infrastructure.vnets.management.default_subnet.nsg.name
-  sap_admin_nsg_existing       = var.infrastructure.vnets.sap.db_admin_subnet.nsg.is_existing
-  sap_admin_nsg_name           = var.infrastructure.vnets.sap.db_admin_subnet.nsg.name
-  sap_client_nsg_existing      = var.infrastructure.vnets.sap.db_client_subnet.nsg.is_existing
-  sap_client_nsg_name          = var.infrastructure.vnets.sap.db_client_subnet.nsg.name
+  sap_admin_nsg_existing       = var.infrastructure.vnets.sap.admin_subnet.nsg.is_existing
+  sap_admin_nsg_name           = var.infrastructure.vnets.sap.admin_subnet.nsg.name
+  sap_client_nsg_existing      = var.infrastructure.vnets.sap.client_subnet.nsg.is_existing
+  sap_client_nsg_name          = var.infrastructure.vnets.sap.client_subnet.nsg.name
   sap_app_nsg_existing         = var.infrastructure.vnets.sap.app_subnet.nsg.is_existing
   sap_app_nsg_name             = var.infrastructure.vnets.sap.app_subnet.nsg.name
 }
