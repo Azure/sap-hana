ba8cecfe1392a42480a9e5bceaf2e6460db767b2
Peter Choo <pchoo@centiq.co.uk>
2020-03-04 15:41:40 +0000

    Update names for lb components

--- deploy/v2/terraform/modules/azure_sap_naming_conventions/outputs.tf
+++ deploy/v2/terraform/modules/azure_sap_naming_conventions/outputs.tf
@@ -13,5 +13,9 @@ output "lb_fe_ip_conf" {
 }
 
 output "lb_be_pool" {
-  value = local.short_name
+  value = "${local.short_name}-bep"
+}
+
+output "lb_health_probe" {
+  value = "${local.short_name}-hp"
 }
