cf2ce40a393cbf5e1ef8170eadf75478935d9742
Peter Choo <pchoo@centiq.co.uk>
2020-03-04 15:43:19 +0000

    Create LB Health Probe based on Instance Number

--- deploy/v2/terraform/modules/hdb_load_balancer/variables.tf
+++ deploy/v2/terraform/modules/hdb_load_balancer/variables.tf
@@ -9,3 +9,11 @@ variable "subnet_id" {
 
 variable "frontend_ip" {
 }
+
+# The HANA DB Instance Number is used as part of the ports for the forwarding rules and
+# Health Probe listener
+variable "instance_number" {
+  type        = string
+  description = "The HANA DB Instance Number"
+  default     = "00"
+}
