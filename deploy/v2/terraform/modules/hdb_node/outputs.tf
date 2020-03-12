3935e57518004a29110450d435250b155418f20b
nancyc <nancyc@microsoft.com>
2020-03-12 12:25:51 -0700

    remove duplicate lb in output from rebase

--- deploy/v2/terraform/modules/hdb_node/outputs.tf
+++ deploy/v2/terraform/modules/hdb_node/outputs.tf
@@ -6,15 +6,11 @@ output "nics-dbnodes-db" {
   value = azurerm_network_interface.nics-dbnodes-db
 }
 
+output "loadbalancers" {
+  value = azurerm_lb.hana-lb
+}
+
 # Workaround to create dependency betweeen ../main.tf ansible_execution and module hdb_node
 output "dbnode-data-disk-att" {
   value = azurerm_virtual_machine_data_disk_attachment.vm-dbnode-data-disk
-}
-
-output "loadbalancers" {
-  value = azurerm_lb.hana-lb
-}
-
-output "loadbalancers" {
-  value = azurerm_lb.hana-lb
 }
