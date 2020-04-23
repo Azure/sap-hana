77fd7496508866451c3cc3d5e73719318fcab703
Morgan Deegan <morgan.deegan@microsoft.com>
2020-04-23 05:41:19 -0700

    Terraform: SAP Library: restructure path

--- deploy/v2/terraform2/workspace/SAP_Library/TFE/module.tf
+++ deploy/v2/terraform2/workspace/SAP_Library/TFE/module.tf
@@ -4,7 +4,7 @@
 |                                                                              |
 +--------------------------------------4--------------------------------------*/
 module "sap_library" {
-  source                                = "../../../../modules/sap_library"
+  source                                = "../../../modules/sap_library"
   deployZone                            = var.deployZone
   region                                = var.region
   tags                                  = var.tags
