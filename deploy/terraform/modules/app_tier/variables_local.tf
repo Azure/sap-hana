5d701028488d75fb1a97e7623d0b7821a2729d39
Peter Choo <pchoo@centiq.co.uk>
2020-06-08 16:50:37 +0100

    Default instance numbers for app tier to prevent issues with old input json files

--- deploy/terraform/modules/app_tier/variables_local.tf
+++ deploy/terraform/modules/app_tier/variables_local.tf
@@ -13,24 +13,27 @@ variable "storage-bootdiag" {
 locals {
   enable_deployment = lookup(var.application, "enable_deployment", false)
 
+  scs_instance_number = lookup(var.application, "scs_instance_number", "01")
+  ers_instance_number = lookup(var.application, "scs_instance_number", "02")
+
   # Ports used for specific ASCS and ERS
   lb-ports = {
     "scs" = [
-      3200 + tonumber(var.application.scs_instance_number),           # e.g. 3201
-      3600 + tonumber(var.application.scs_instance_number),           # e.g. 3601
-      3900 + tonumber(var.application.scs_instance_number),           # e.g. 3901
-      8100 + tonumber(var.application.scs_instance_number),           # e.g. 8101
-      50013 + (tonumber(var.application.scs_instance_number) * 100),  # e.g. 50113
-      50014 + (tonumber(var.application.scs_instance_number) * 100),  # e.g. 50114
-      50016 + (tonumber(var.application.scs_instance_number) * 100),  # e.g. 50116
+      3200 + tonumber(local.scs_instance_number),           # e.g. 3201
+      3600 + tonumber(local.scs_instance_number),           # e.g. 3601
+      3900 + tonumber(local.scs_instance_number),           # e.g. 3901
+      8100 + tonumber(local.scs_instance_number),           # e.g. 8101
+      50013 + (tonumber(local.scs_instance_number) * 100),  # e.g. 50113
+      50014 + (tonumber(local.scs_instance_number) * 100),  # e.g. 50114
+      50016 + (tonumber(local.scs_instance_number) * 100),  # e.g. 50116
     ]
 
     "ers" = [
-      3200 + tonumber(var.application.ers_instance_number),          # e.g. 3202
-      3300 + tonumber(var.application.ers_instance_number),          # e.g. 3302
-      50013 + (tonumber(var.application.ers_instance_number) * 100), # e.g. 50213
-      50014 + (tonumber(var.application.ers_instance_number) * 100), # e.g. 50214
-      50016 + (tonumber(var.application.ers_instance_number) * 100), # e.g. 50216
+      3200 + tonumber(local.ers_instance_number),          # e.g. 3202
+      3300 + tonumber(local.ers_instance_number),          # e.g. 3302
+      50013 + (tonumber(local.ers_instance_number) * 100), # e.g. 50213
+      50014 + (tonumber(local.ers_instance_number) * 100), # e.g. 50214
+      50016 + (tonumber(local.ers_instance_number) * 100), # e.g. 50216
     ]
   }
 
@@ -39,7 +42,7 @@ locals {
   # SCS (index 0) - 620nn
   # ERS (index 1) - 621nn
   hp-ports = [
-    62000 + tonumber(var.application.scs_instance_number),
-    62100 + tonumber(var.application.ers_instance_number)
+    62000 + tonumber(local.scs_instance_number),
+    62100 + tonumber(local.ers_instance_number)
   ]
 }
