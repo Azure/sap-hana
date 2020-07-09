c1a6c883eab561893fb6ba43259c877e35fd1f54
Kimmo Forss <kimmo.forss@microsoft.com>
2020-07-09 23:48:11 +0300

    bug fixes

--- deploy/terraform/modules/anydb_node/loadbalancer.tf
+++ deploy/terraform/modules/anydb_node/loadbalancer.tf
@@ -4,14 +4,15 @@
 Load balancer front IP address range: .4 - .9
 +--------------------------------------4--------------------------------------*/
 
-resource "azurerm_lb" "anydb" {
+
+resource "azurerm_lb" "lb" {
   count               = local.enable_deployment ? 1 : 0
-  name                = format("%s-%s-lb", var.role, local.prefix)
+  name                = format("%s-%s-lb", var.role, local.sid)
   resource_group_name = var.resource-group[0].name
   location            = var.resource-group[0].location
 
   frontend_ip_configuration {
-    name = format("%s-%s-lb-feip", var.role, local.prefix)
+    name = format("%s-%s-lb-feip", var.role, local.sid)
 
     subnet_id          = var.infrastructure.vnets.sap.subnet_db.is_existing ? data.azurerm_subnet.anydb[0].id : azurerm_subnet.anydb[0].id
     private_ip_address = var.infrastructure.vnets.sap.subnet_db.is_existing ? try(local.any-databases.loadbalancer.frontend_ip, cidrhost(var.infrastructure.vnets.sap.subnet_db.prefix, tonumber(count.index) + 4)) : cidrhost(var.infrastructure.vnets.sap.subnet_db.prefix, tonumber(count.index) + 4)
@@ -22,7 +23,7 @@ resource "azurerm_lb" "anydb" {
 
 resource "azurerm_lb_backend_address_pool" "lb-back-pool" {
   count               = local.enable_deployment ? 1 : 0
-  name                = format("%s-%s-lb-bep", var.role, local.prefix)
+  name                = format("%s-%s-lb-bep", var.role, local.sid)
   resource_group_name = var.resource-group[0].name
   loadbalancer_id     = azurerm_lb.anydb[0].id
 
@@ -31,8 +32,8 @@ resource "azurerm_lb_backend_address_pool" "lb-back-pool" {
 resource "azurerm_lb_probe" "lb-health-probe" {
   count               = local.enable_deployment ? 1 : 0
   resource_group_name = var.resource-group[0].name
-  loadbalancer_id     = azurerm_lb.anydb[0].id
-  name                = format("%s-%s-lb-hpp", var.role, local.prefix)
+  loadbalancer_id     = azurerm_lb.lb[0].id
+  name                = format("%s-%s-lb-hpp", var.role, local.sid)
   port                = (local.anydb_platform == "DB2") ? "62500" : "1521"
   protocol            = "Tcp"
   interval_in_seconds = 5
@@ -54,7 +55,7 @@ resource "azurerm_lb_rule" "lb-rules" {
   protocol                       = "Tcp"
   frontend_port                  = local.loadbalancers[0].ports[count.index]
   backend_port                   = local.loadbalancers[0].ports[count.index]
-  frontend_ip_configuration_name = format("%s-%s-lb-feip", var.role, local.prefix)
+  frontend_ip_configuration_name = format("%s-%s-lb-feip", var.role, local.sid)
   backend_address_pool_id        = azurerm_lb_backend_address_pool.lb-back-pool[0].id
   probe_id                       = azurerm_lb_probe.lb-health-probe[0].id
   enable_floating_ip             = true
