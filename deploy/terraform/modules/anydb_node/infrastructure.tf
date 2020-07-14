070ad7387c4720221f95a4d272b51f7a72d76604
Kimmo Forss <kimmo.forss@microsoft.com>
2020-07-14 21:10:17 +0300

    Update deploy/terraform/modules/anydb_node/infrastructure.tf
    Co-authored-by: Nancy Chen <38501271+rukawata@users.noreply.github.com>

--- deploy/terraform/modules/anydb_node/infrastructure.tf
+++ deploy/terraform/modules/anydb_node/infrastructure.tf
@@ -16,7 +16,7 @@ resource "azurerm_lb" "anydb" {
   }
 }
 
-resource "azurerm_lb_backend_address_pool" "lb-back-pool" {
+resource "azurerm_lb_backend_address_pool" "anydb" {
   count               = local.enable_deployment ? 1 : 0
   resource_group_name = var.resource-group[0].name
   loadbalancer_id     = azurerm_lb.anydb[count.index].id
