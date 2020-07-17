7c061e2b31f01703a028758640360899cda21713
Kimmo Forss <kimmo.forss@microsoft.com>
2020-07-17 09:55:07 +0300

    PR Fixes

--- deploy/terraform/modules/anydb_node/vm-anydb.tf
+++ deploy/terraform/modules/anydb_node/vm-anydb.tf
@@ -53,6 +53,7 @@ resource azurerm_linux_virtual_machine "dbserver" {
 
   computer_name                   = lower(format("%sxdbl%02d", upper(local.anydb_sid), count.index))
   admin_username                  = local.authentication.username
+  admin_password                  = local.authentication.password
   disable_password_authentication = local.authentication.type != "password" ? true : false
 
   admin_ssh_key {
