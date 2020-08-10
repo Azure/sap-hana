53ca44140122f8de17ebe9a58b47375bd4ce917b
rukawata <rukawata@gmail.com>
2020-08-10 09:54:57 -0700

    Change file/folder name to use underscore instead of hyphen

--- deploy/terraform/terraform-units/modules/sap_system/saplibrary/remote_file.tf
+++ deploy/terraform/terraform-units/modules/sap_system/saplibrary/remote_file.tf
@@ -5,9 +5,9 @@
 
 // Upload sapsystem.json to json container
 resource "azurerm_storage_blob" "sapsystem_json" {
-  name                   = "${local.landscape_id}/${local.sid}/${local.landscape_id}-${local.sid}.json"
+  name                   = "${local.landscape_id}/${local.sid}/${local.landscape_id}_${local.sid}.json"
   storage_account_name   = local.sa_tfstate.name
   storage_container_name = local.storagecontainer_sapsystem.name
   type                   = "Block"
-  source                 = "${path.root}/${local.landscape_id}-${local.sid}.json"
+  source                 = "${path.root}/${local.landscape_id}_${local.sid}.json"
 }
