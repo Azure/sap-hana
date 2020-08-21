3a55d90dfe5acc5e0af2ac05d3ee9575083678d2
rukawata <rukawata@gmail.com>
2020-08-21 08:13:28 -0700

    Address new requirement for saplib

--- deploy/terraform/bootstrap/sap_library/output.tf
+++ deploy/terraform/bootstrap/sap_library/output.tf
@@ -1,31 +1,19 @@
 output "tfstate_storage_account" {
-    value = module.sap_library.tfstate_storage_account
+  value = module.sap_library.tfstate_storage_account
 }
 
 output "sapbits_storage_account" {
-    value = module.sap_library.sapbits_storage_account
-}
-
-output "storagecontainer_sapsystem" {
-    value = module.sap_library.storagecontainer_sapsystem
-}
-
-output "storagecontainer_saplandscape" {
-    value = module.sap_library.storagecontainer_saplandscape
-}
-
-output "storagecontainer_deployer" {
-    value = module.sap_library.storagecontainer_deployer
+  value = module.sap_library.sapbits_storage_account
 }
 
 output "storagecontainer_saplibrary" {
-    value = module.sap_library.storagecontainer_saplibrary
+  value = module.sap_library.storagecontainer_saplibrary
 }
 
 output "storagecontainer_sapbits" {
-    value = module.sap_library.storagecontainer_sapbits
+  value = module.sap_library.storagecontainer_sapbits
 }
 
 output "fileshare_sapbits_name" {
-    value = module.sap_library.fileshare_sapbits_name
+  value = module.sap_library.fileshare_sapbits_name
 }
