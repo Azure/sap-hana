7e4f2bc17d3b9258f6c4c57b9b63ef1350e24457
Yunzi Zhang <yunzzha@microsoft.com>
2020-09-22 10:19:13 -0700

    format key vault tf

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/key_vault.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/key_vault.tf
@@ -7,19 +7,19 @@ data "azurerm_client_config" "deployer" {}
 
 // Create private KV with access policy
 resource "azurerm_key_vault" "kv_prvt" {
-  count                      = local.enable_deployment ? 1 : 0
-  name                       = local.kv_private_name
-  location                   = local.region
-  resource_group_name        = var.resource-group[0].name
-  tenant_id                  = data.azurerm_client_config.deployer.tenant_id
-  soft_delete_enabled        = true
+  count = local.enable_deployment ? 1 : 0
+  name = local.kv_private_name
+  location = local.region
+  resource_group_name = var.resource-group[0].name
+  tenant_id = data.azurerm_client_config.deployer.tenant_id
+  soft_delete_enabled = true
   soft_delete_retention_days = 7
-  purge_protection_enabled   = true
-  sku_name                   = "standard"
+  purge_protection_enabled = true
+  sku_name = "standard"
 }
 
 resource "azurerm_key_vault_access_policy" "kv_prvt_msi" {
-  count        = local.enable_deployment ? 1 : 0
+  count = local.enable_deployment ? 1 : 0
   key_vault_id = azurerm_key_vault.kv_prvt[0].id
 
   tenant_id = data.azurerm_client_config.deployer.tenant_id
@@ -32,20 +32,20 @@ resource "azurerm_key_vault_access_policy" "kv_prvt_msi" {
 
 // Create user KV with access policy
 resource "azurerm_key_vault" "kv_user" {
-  count                      = local.enable_deployment ? 1 : 0
-  name                       = local.kv_user_name
-  location                   = local.region
-  resource_group_name        = var.resource-group[0].name
-  tenant_id                  = data.azurerm_client_config.deployer.tenant_id
-  soft_delete_enabled        = true
+  count = local.enable_deployment ? 1 : 0
+  name = local.kv_user_name
+  location = local.region
+  resource_group_name = var.resource-group[0].name
+  tenant_id = data.azurerm_client_config.deployer.tenant_id
+  soft_delete_enabled = true
   soft_delete_retention_days = 7
-  purge_protection_enabled   = true
+  purge_protection_enabled = true
 
   sku_name = "standard"
 }
 
 resource "azurerm_key_vault_access_policy" "kv_user_msi" {
-  count        = local.enable_deployment ? 1 : 0
+  count = local.enable_deployment ? 1 : 0
   key_vault_id = azurerm_key_vault.kv_user[0].id
   tenant_id = data.azurerm_client_config.deployer.tenant_id
   object_id = var.deployer-uai.principal_id
@@ -59,7 +59,7 @@ resource "azurerm_key_vault_access_policy" "kv_user_msi" {
 }
 
 resource "azurerm_key_vault_access_policy" "kv_user_portal" {
-  count        = local.enable_deployment ? length(local.kv_users) : 0
+  count = local.enable_deployment ? length(local.kv_users) : 0
   key_vault_id = azurerm_key_vault.kv_user[0].id
   tenant_id = data.azurerm_client_config.deployer.tenant_id
   object_id = local.kv_users[count.index]
@@ -77,8 +77,8 @@ resource "random_password" "password" {
   count = (
   local.enable_auth_password
   && try(local.authentication.password, null) == null ) ? 1 : 0
-  length           = 16
-  special          = true
+  length = 16
+  special = true
   override_special = "_%@"
 }
 
@@ -93,18 +93,20 @@ resource "random_id" "sapsystem" {
 */
 // for logon use
 resource "azurerm_key_vault_secret" "auth_username" {
-  depends_on   = [azurerm_key_vault_access_policy.kv_user_msi]
-  count        = local.enable_auth_password ? 1 : 0
-  name         = format("%s-sid-logon-username", local.prefix)
-  value        = local.sid_auth_username
+  depends_on = [
+    azurerm_key_vault_access_policy.kv_user_msi]
+  count = local.enable_auth_password ? 1 : 0
+  name = format("%s-sid-logon-username", local.prefix)
+  value = local.sid_auth_username
   key_vault_id = azurerm_key_vault.kv_user[0].id
 }
 
 // for logon use
 resource "azurerm_key_vault_secret" "auth_password" {
-  depends_on   = [azurerm_key_vault_access_policy.kv_user_msi]
-  count        = local.enable_auth_password ? 1 : 0
-  name         = format("%s-sid-logon-password", local.prefix)
-  value        = local.sid_auth_password
+  depends_on = [
+    azurerm_key_vault_access_policy.kv_user_msi]
+  count = local.enable_auth_password ? 1 : 0
+  name = format("%s-sid-logon-password", local.prefix)
+  value = local.sid_auth_password
   key_vault_id = azurerm_key_vault.kv_user[0].id
 }
