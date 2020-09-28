fad95212d1183b8cf8d53fe8a100e7fc5862f365
Kimmo Forss <kimmo.forss@microsoft.com>
2020-09-28 21:06:46 +0300

    PR comments

--- deploy/terraform/terraform-units/modules/sap_library/configuration_output.tf
+++ deploy/terraform/terraform-units/modules/sap_library/configuration_output.tf
@@ -2,10 +2,13 @@
 # OUTPUT Files
 ##################################################################################################################
 
-# Generates the output JSON with IP address and disk details
 resource local_file "backend" {
-  content = templatefile("${path.module}/backend.tmpl", {rg_name=local.rg_name,sa_tfstate=local.sa_tfstate_name})
-  filename             = "${path.cwd}/backend.txt"
+  content = templatefile("${path.module}/backend.tmpl",
+    {
+      rg_name    = local.rg_name,
+      sa_tfstate = local.sa_tfstate_name
+  })
+  filename             = "${path.cwd}/backend-config.tfvars"
   file_permission      = "0660"
   directory_permission = "0770"
 }
