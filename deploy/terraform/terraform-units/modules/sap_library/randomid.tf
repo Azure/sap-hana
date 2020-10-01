8e41cdb1525386edfab7279340fadfeed1d6c153
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-01 10:14:20 +0300

    Added back the keeper to assist in the pipeline

--- deploy/terraform/terraform-units/modules/sap_library/randomid.tf
+++ deploy/terraform/terraform-units/modules/sap_library/randomid.tf
@@ -1,4 +1,9 @@
 # Generates random text for boot diagnostics storage account name
-resource "random_id" lib_random_id {
+resource "random_id"  "post_fix" {
+  keepers = {
+    // Generate a new id only when a new resource group is defined
+    resource_group = local.rg_exists ? data.azurerm_resource_group.library[0].name : azurerm_resource_group.library[0].name
+  }
   byte_length = 4
 }
+ 
