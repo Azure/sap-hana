8f85c288707dc523bf16ce194e75550f9eabbf55
Kimmo Forss <kimmo.forss@microsoft.com>
2020-10-01 10:19:01 +0300

    removed the keeper to remove the cycle

--- deploy/terraform/terraform-units/modules/sap_library/randomid.tf
+++ deploy/terraform/terraform-units/modules/sap_library/randomid.tf
@@ -1,9 +1,5 @@
 # Generates random text for boot diagnostics storage account name
 resource "random_id"  "post_fix" {
-  keepers = {
-    // Generate a new id only when a new resource group is defined
-    resource_group = local.rg_exists ? data.azurerm_resource_group.library[0].name : azurerm_resource_group.library[0].name
-  }
   byte_length = 4
 }
  
