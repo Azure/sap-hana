203aed1cff7ecfb4d0c547e0287c9ec1ee06f79f
Yunzi Zhang <yunzzha@microsoft.com>
2020-11-06 20:27:07 -0800

    add output

--- deploy/terraform/run/sap_landscape/output.tf
+++ deploy/terraform/run/sap_landscape/output.tf
@@ -1,7 +1,19 @@
-output "saplandscape_kv_user_arm_id" {
+output "vnet_resource_group_name" {
+  value = try(module.sap_landscape.vnet_sap[0].resource_group_name, "")
+}
+
+output "vet_sap_name" {
+  value = try(module.sap_landscape.vnet_sap[0].name, "")
+}
+
+output "landscape_key_vault_user_arm_id" {
   value = try(module.sap_landscape.kv_user[0].id, "")
 }
 
 output "sid_public_key_secret_name" {
   value = try(module.sap_landscape.sid_public_key_secret_name, "")
+}
+
+output "nics_iscsi" {
+  value = try(module.sap_landscape.nics_iscsi[*].private_ip_address, [])
 }
