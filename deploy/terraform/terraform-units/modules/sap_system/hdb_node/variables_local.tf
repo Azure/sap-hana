c2999505521760759feef753fcb33c4bf52a1ea7
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-10 20:11:53 +0200

    duplicate variables

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/variables_local.tf
@@ -36,10 +36,6 @@ variable "sid_kv_user" {
   description = "Details of the user keyvault for sap_system"
 }
 
-variable "sid_kv_user" {
-  description = "Details of the user keyvault for sap_system"
-}
-
 locals {
   // Imports database sizing information
 
