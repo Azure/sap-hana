5861acf8ed689561d3144b8735d94c179aa19119
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-18 19:46:29 +0200

    Add a third nic for ANF scaleout

--- deploy/terraform/terraform-units/modules/sap_system/hdb_node/vm-hdb.tf
+++ deploy/terraform/terraform-units/modules/sap_system/hdb_node/vm-hdb.tf
@@ -59,6 +59,56 @@ resource "azurerm_network_interface" "nics_dbnodes_db" {
   }
 }
 
+// Creates the NIC for Hana storage
+resource "azurerm_network_interface" "nics_dbnodes_storage" {
+  count = local.enable_deployment && local.enable_storage_subnet ? length(local.hdb_vms) : 0
+  name  = format("%s%s", local.hdb_vms[count.index].name, local.resource_suffixes.storage_nic)
+
+  location                      = var.resource_group[0].location
+  resource_group_name           = var.resource_group[0].name
+  enable_accelerated_networking = true
+
+  ip_configuration {
+    primary = true
+    name    = "ipconfig1"
+
+    subnet_id = local.sub_storage_exists ? data.azurerm_subnet.storage[0].id : azurerm_subnet.storage[0].id
+
+    private_ip_address = local.use_DHCP ? null : try(local.hdb_vms[count.index].storage_nic_ip, false) != false ? (
+      local.hdb_vms[count.index].storage_nic_ip) : (
+      cidrhost(local.sub_storage_exists ? (
+        data.azurerm_subnet.storage[0].address_prefixes[0]) : (
+        azurerm_subnet.storage[0].address_prefixes[0]
+      ), tonumber(count.index) + local.hdb_ip_offsets.hdb_storage_vm)
+
+    )
+    private_ip_address_allocation = local.use_DHCP ? "Dynamic" : "Static"
+  }
+}
+
+// Creates the NIC for Hana storage
+resource "azurerm_network_interface" "nics_dbnodes_storage" {
+  count = local.enable_deployment && local.storage_subnet_needed ? length(local.hdb_vms) : 0
+  name  = format("%s%s", local.hdb_vms[count.index].name, local.resource_suffixes.storage_nic)
+
+  location                      = var.resource_group[0].location
+  resource_group_name           = var.resource_group[0].name
+  enable_accelerated_networking = true
+
+  ip_configuration {
+    primary   = true
+    name      = "ipconfig1"
+    subnet_id = var.storage_subnet.id
+
+    private_ip_address = try(local.hdb_vms[count.index].storage_nic_ip, false) != false ? (
+      local.hdb_vms[count.index].storage_nic_ip) : (
+      cidrhost(var.storage_subnet.address_prefixes[0], tonumber(count.index) + local.hdb_ip_offsets.hdb_storage_vm)
+    )
+    private_ip_address_allocation = local.use_DHCP ? "Dynamic" : "Static"
+  }
+}
+
+
 # LOAD BALANCER ===================================================================================================
 
 /*-----------------------------------------------------------------------------8
@@ -152,10 +202,13 @@ resource "azurerm_linux_virtual_machine" "vm_dbnode" {
 
   zone = local.enable_ultradisk || local.db_server_count == local.db_zone_count ? local.zones[count.index % max(local.db_zone_count, 1)] : null
 
-  network_interface_ids = [
+  network_interface_ids = local.enable_storage_subnet ? ([
     azurerm_network_interface.nics_dbnodes_admin[count.index].id,
-    azurerm_network_interface.nics_dbnodes_db[count.index].id
-  ]
+    azurerm_network_interface.nics_dbnodes_db[count.index].id,
+    azurerm_network_interface.nics_dbnodes_storage[count.index].id]) : ([
+    azurerm_network_interface.nics_dbnodes_admin[count.index].id,
+    azurerm_network_interface.nics_dbnodes_db[count.index].id]
+  )
   size                            = lookup(local.sizes, local.hdb_vms[count.index].size).compute.vm_size
   admin_username                  = local.sid_auth_username
   admin_password                  = local.sid_auth_password
