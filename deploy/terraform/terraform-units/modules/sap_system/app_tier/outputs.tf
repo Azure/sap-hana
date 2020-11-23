6cda04453237f89bd9454f976cfc4315f9290800
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-24 01:09:15 +0200

    change output to map

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/outputs.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/outputs.tf
@@ -65,67 +65,53 @@ output "application" {
 // Output for DNS
 output "dns_info_vms" {
   value = local.enable_deployment ? (
-    concat(local.apptier_dual_nics ? (
-      flatten([for idx, vm in local.app_virtualmachine_names :
-        {
-          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.app_admin[idx].private_ip_address,
-          var.naming.virtualmachine_names.APP_SECONDARY_DNSNAME[idx]                             = azurerm_network_interface.app[idx].private_ip_address
-
-        }
-      ])) : (
-      flatten([for idx, vm in local.app_virtualmachine_names :
-        {
-          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.app[idx].private_ip_address,
-        }
-      ])
-      ), local.apptier_dual_nics ? (
-      flatten([for idx, vm in local.scs_virtualmachine_names :
-        {
-          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.scs_admin[idx].private_ip_address,
-          var.naming.virtualmachine_names.SCS_SECONDARY_DNSNAME[idx]                             = azurerm_network_interface.scs[idx].private_ip_address
-
-        }
-      ])) : (
-      flatten([for idx, vm in local.scs_virtualmachine_names :
-        {
-          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.scs[idx].private_ip_address,
-        }
-      ])
-      ), local.apptier_dual_nics ? (
-      flatten([for idx, vm in local.web_virtualmachine_names :
-        {
-          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.web_admin[idx].private_ip_address,
-          var.naming.virtualmachine_names.SCS_SECONDARY_DNSNAME[idx]                             = azurerm_network_interface.web[idx].private_ip_address
-
-        }
-      ])) : (
-      flatten([for idx, vm in local.web_virtualmachine_names :
-        {
-          format("%s%s%s%s", local.prefix, var.naming.separator, vm, local.resource_suffixes.vm) = azurerm_network_interface.web[idx].private_ip_address,
-        }
-      ])
-      )
-    )) : (
+    local.apptier_dual_nics ? (
+      zipmap(
+        concat(local.full_appserver_names,
+          var.naming.virtualmachine_names.APP_SECONDARY_DNSNAME,
+          local.full_scsserver_names,
+          var.naming.virtualmachine_names.SCS_SECONDARY_DNSNAME,
+          local.full_webserver_names,
+          var.naming.virtualmachine_names.WEB_SECONDARY_DNSNAME
+        ),
+        concat(azurerm_network_interface.app_admin[*].private_ip_address,
+          azurerm_network_interface.app[*].private_ip_address,
+          azurerm_network_interface.scs_admin[*].private_ip_address,
+          azurerm_network_interface.scs[*].private_ip_address,
+          azurerm_network_interface.web_admin[*].private_ip_address,
+          azurerm_network_interface.web[*].private_ip_address
+      ))) : (
+      zipmap(
+        concat(local.full_appserver_names,
+          local.full_scsserver_names,
+          local.full_webserver_names
+        ),
+        concat(azurerm_network_interface.app[*].private_ip_address,
+          azurerm_network_interface.scs[*].private_ip_address,
+          azurerm_network_interface.web[*].private_ip_address
+      ))
+    )
+    ) : (
     null
   )
 }
 
 output "dns_info_loadbalancers" {
-  value = local.enable_deployment ? (
-    [
+  value = zipmap(
+    concat((
       local.scs_server_count > 0 ? (
-        { format("%s%s%s", local.prefix, var.naming.separator, "scs") = azurerm_lb.scs[0].private_ip_addresses[0] }) : (
-        null
-      ),
-      local.scs_server_count > 0 ? (
-        { format("%s%s%s", local.prefix, var.naming.separator, "ers") = azurerm_lb.scs[0].private_ip_addresses[1] }) : (
-        null
-      ),
-      local.webdispatcher_count > 0 ? (
-        { format("%s%s%s", local.prefix, var.naming.separator, local.resource_suffixes.web_alb) = azurerm_lb.web[0].private_ip_addresses[0] }) : (
+        concat(format("%s%s%s", local.prefix, var.naming.separator, "scs"), format("%s%s%s", local.prefix, var.naming.separator, "ers"))) : (
         null
       )
-    ]) : (
-    null
+      ), (
+      local.webdispatcher_count > 0 ? format("%s%s%s", local.prefix, var.naming.separator, local.resource_suffixes.web_alb) : null)
+    ),
+    concat((
+      local.scs_server_count > 0 ? (
+        concat(azurerm_lb.scs[0].private_ip_addresses[0], azurerm_lb.scs[0].private_ip_addresses[1])) : (
+        null
+      )), (
+      local.webdispatcher_count > 0 ? azurerm_lb.web[0].private_ip_addresses[0] : null)
+    )
   )
 }
