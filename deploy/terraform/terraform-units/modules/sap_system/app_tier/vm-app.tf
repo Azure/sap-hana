68cbd1a8bc50fb11db9a8cfadb249b85aafd07f5
Kimmo Forss <kimmo.forss@microsoft.com>
2020-11-25 21:01:46 +0200

    v1 of tags

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-app.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/vm-app.tf
@@ -128,6 +128,8 @@ resource "azurerm_linux_virtual_machine" "app" {
   boot_diagnostics {
     storage_account_uri = var.storage_bootdiag.primary_blob_endpoint
   }
+
+  tags = local.app_tags
 }
 
 # Create the Windows Application VM(s)
@@ -182,6 +184,8 @@ resource "azurerm_windows_virtual_machine" "app" {
   boot_diagnostics {
     storage_account_uri = var.storage_bootdiag.primary_blob_endpoint
   }
+
+  tags = local.app_tags
 }
 
 # Creates managed data disk
