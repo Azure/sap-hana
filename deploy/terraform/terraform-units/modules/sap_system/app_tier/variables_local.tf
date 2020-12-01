0c017e2e628e9d4a0944a740a62cc3a07762f7f9
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-01 19:28:28 +0200

    PR Comments

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/variables_local.tf
@@ -207,9 +207,9 @@ locals {
     "version"         = try(var.application.scs_os.version, local.scs_custom_image ? "" : local.app_os.version)
   }
 
-  app_tags = try(var.application.app_tags,{})
-  scs_tags = try(var.application.scs_tags,{})
-  web_tags = try(var.application.web_tags,{})
+  app_tags = try(var.application.app_tags, {})
+  scs_tags = try(var.application.scs_tags, {})
+  web_tags = try(var.application.web_tags, {})
   application = merge(var.application,
     { authentication = local.authentication }
   )
