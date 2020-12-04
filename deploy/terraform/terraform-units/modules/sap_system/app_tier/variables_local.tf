ba8bd0eb27a0b856a306d0afb6909545c6792485
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-05 00:54:53 +0200

    corrected the local.offset variable

--- deploy/terraform/terraform-units/modules/sap_system/app_tier/variables_local.tf
+++ deploy/terraform/terraform-units/modules/sap_system/app_tier/variables_local.tf
@@ -335,7 +335,7 @@ locals {
     [
       for storage_type in local.app_sizing.storage : [
         for disk_count in range(storage_type.count) : {
-          suffix               = format("-%s%02d", storage_type.name, disk_count + var.resource_offset)
+          suffix               = format("-%s%02d", storage_type.name, disk_count + local.offset)
           storage_account_type = storage_type.disk_type,
           disk_size_gb         = storage_type.size_gb,
           //The following two lines are for Ultradisks only
@@ -369,7 +369,7 @@ locals {
     [
       for storage_type in local.scs_sizing.storage : [
         for disk_count in range(storage_type.count) : {
-          suffix               = format("-%s%02d", storage_type.name, disk_count + var.resource_offset)
+          suffix               = format("-%s%02d", storage_type.name, disk_count + local.offset)
           storage_account_type = storage_type.disk_type,
           disk_size_gb         = storage_type.size_gb,
           //The following two lines are for Ultradisks only
@@ -403,7 +403,7 @@ locals {
     [
       for storage_type in local.web_sizing.storage : [
         for disk_count in range(storage_type.count) : {
-          suffix               = format("-%s%02d", storage_type.name, disk_count + var.resource_offset)
+          suffix               = format("-%s%02d", storage_type.name, disk_count + local.offset)
           storage_account_type = storage_type.disk_type,
           disk_size_gb         = storage_type.size_gb,
           //The following two lines are for Ultradisks only
