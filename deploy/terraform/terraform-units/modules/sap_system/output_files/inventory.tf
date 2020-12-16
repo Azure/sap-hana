86007f1622f1a8598931cd71b05b2e4ce5af1af7
Kimmo Forss <kimmo.forss@microsoft.com>
2020-12-16 21:27:59 +0200

    New YML

--- deploy/terraform/terraform-units/modules/sap_system/output_files/inventory.tf
+++ deploy/terraform/terraform-units/modules/sap_system/output_files/inventory.tf
@@ -145,7 +145,7 @@ resource "local_file" "ansible_inventory_new_yml" {
   directory_permission = "0770"
 }
 
-resource "azurerm_storage_blob" "example" {
+resource "azurerm_storage_blob" "hostsyaml" {
 
   name                   = format("%s_hosts.yml", trimspace(var.naming.prefix.SDU))
   storage_account_name   = local.tfstate_storage_account_name
@@ -153,4 +153,5 @@ resource "azurerm_storage_blob" "example" {
   storage_container_name = local.ansible_container_name
   type                   = "Block"
   source                 = local_file.ansible_inventory_new_yml.filename
+}
 }
