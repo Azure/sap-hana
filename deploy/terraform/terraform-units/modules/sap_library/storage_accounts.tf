988200a510817824a15e4ca7ae73f8a7ceb5113d
Kimmo Forss <kimmo.forss@microsoft.com>
2021-01-04 10:36:56 +0200

    code fixes

--- deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
+++ deploy/terraform/terraform-units/modules/sap_library/storage_accounts.tf
@@ -98,26 +98,16 @@ resource "azurerm_role_assignment" "deployer_msi_sa_tfstate" {
   principal_id         = local.deployer_msi_principal_id
 }
 
-// Creates storage account for storing ansible YML files
-resource "azurerm_storage_account" "storage_ansible" {
-  count                     = local.sa_ansible_exists ? 0 : 1
-  name                      = local.sa_ansible_name
-  resource_group_name       = local.rg_name
-  location                  = local.rg_library_location
-  account_replication_type  = local.sa_tfstate_account_replication_type
-  account_tier              = local.sa_tfstate_account_tier
-  account_kind              = local.sa_tfstate_account_kind
-  enable_https_traffic_only = local.sa_tfstate_enable_secure_transfer
-  allow_blob_public_access  = true
-  blob_properties {
-    delete_retention_policy {
-      days = 7
-    }
-  }
+// Creates the storage container inside the storage account for sapsystem
+resource "azurerm_storage_container" "storagecontainer_ansible" {
+  count                 = local.sa_ansible_container_exists ? 0 : 1
+  name                  = local.sa_ansible_container_name
+  storage_account_name  = local.sa_tfstate_exists ? data.azurerm_storage_account.storage_tfstate[0].name : azurerm_storage_account.storage_tfstate[0].name
+  container_access_type = local.sa_tfstate_container_access_type
 }
 
 data "azurerm_storage_container" "storagecontainer_ansible" {
   count                = local.sa_ansible_container_exists ? 1 : 0
   name                 = local.sa_ansible_container_name
-  storage_account_name = local.sa_ansible_exists ? data.azurerm_storage_account.storage_tfstate[0].name : azurerm_storage_account.storage_tfstate[0].name
+  storage_account_name = local.sa_tfstate_exists ? data.azurerm_storage_account.storage_tfstate[0].name : azurerm_storage_account.storage_tfstate[0].name
 }
