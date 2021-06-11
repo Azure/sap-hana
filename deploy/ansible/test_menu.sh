5552b3cddc132e2e927da20f0f85d72715d708dc
Morgan Deegan <morgan.deegan@microsoft.com>
2021-06-11 06:42:08 -0700

    BOM: Logic for Repo or Storage Account BOMs

--- deploy/ansible/test_menu.sh
+++ deploy/ansible/test_menu.sh
@@ -64,7 +64,7 @@ export           ANSIBLE_PYTHON_INTERPRETER=auto_silent
 
 # Ref: https://docs.ansible.com/ansible/2.9/plugins/callback/default.html
 # Don't show skipped tasks
-#export           ANSIBLE_DISPLAY_SKIPPED_HOSTS=no
+export           ANSIBLE_DISPLAY_SKIPPED_HOSTS=no
 
 # Ref: https://docs.ansible.com/ansible/2.9/plugins/callback/profile_tasks.html
 # Commented out defaults below
